af584d59f0e5293486a8f35265fcba5580fd8795
hagukin <hagukin@naver.com>
2021-08-23 11:49:33 +0900

    add test shaders, camera fov is now affected by velocity

--- Assets/Scripts/Contents/CameraController.cs
+++ Assets/Scripts/Contents/CameraController.cs
@@ -9,16 +9,18 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public Transform rotater;
     public float distance;
-
+    public Player player;
+    private Camera gameCam;
 
     void Start()
     {
-        
+        gameCam = GetComponent<Camera>();
     }
 
     void Update()
     {
-        
+        Debug.Log(player.velocity);
+        gameCam.fieldOfView = Mathf.Clamp(player.velocity*13, 70, 120); // TODO: fov 따라 카메라 distance도 동적으로 변경할지 결정하기.
         transform.position = target.position - transform.forward * distance;
         transform.parent.eulerAngles = rotater.eulerAngles;
     }
